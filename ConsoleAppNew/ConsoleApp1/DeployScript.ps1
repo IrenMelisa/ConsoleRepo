8a181bb8c16f09823aefd4b8fe3d6913117ce561
IrenMelisa <Beshenko.Irina@gmail.com>
2023-11-21 16:46:44 +0100

    Update DeployScript.ps1

--- ConsoleAppNew/ConsoleApp1/DeployScript.ps1
+++ ConsoleAppNew/ConsoleApp1/DeployScript.ps1
@@ -17,6 +17,6 @@ Write-Host "envs: $env:GITHUB_WORKSPACE"
 # Install application
 #  Start-Process -FilePath "msiexec" -ArgumentList "/i ""$msiPath"" /passive /log ""$logFileName"" INSTALLDIR=""${env:ProgramFiles(x86)}\$applicationName\1.0""" -Wait -Passthru | Out-Null
 Invoke-Command -ComputerName dea059yw.eito-dublin.local -ScriptBlock { 
- mkdir "D:\tmp"
+New-Item "D:\IRA" -ItemType Directory
 }
 Write-Host "OUT"
