0b2a7aac4ee4e547c8cd159a3767f2609bfbbe11
IrenMelisa <Beshenko.Irina@gmail.com>
2023-11-21 12:20:32 +0100

    Update DeployScript.ps1

--- ConsoleAppNew/ConsoleApp1/DeployScript.ps1
+++ ConsoleAppNew/ConsoleApp1/DeployScript.ps1
@@ -1,10 +1,9 @@
 $applicationName = "ConsoleApp1"
-# $msBuildExecutable = "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe"
 $projectsDirectory = "$env:GITHUB_WORKSPACE\ConsoleAppNew\ConsoleApp1"
 $ApplicationStartDate = Get-Date
 $configuration = "Debug"
 $applicationLog = "logTMP"
-    $msiFolder = "$env:GITHUB_WORKSPACE\ConsoleAppNew\MSI"
+    $msiFolder = "$env:GITHUB_WORKSPACE\ConsoleAppNew\ConsoleApp1\MSI"
 $maximumRetries = 1
 $programFilesDirectory = "C:\Program Files (x86)"
 
@@ -19,3 +18,8 @@ Write-Host "envs: $env:GITHUB_WORKSPACE"
 # Install application
   Start-Process -FilePath "msiexec" -ArgumentList "/i ""$msiPath"" /passive /log ""$logFileName"" INSTALLDIR=""$programFilesDirectory\$applicationName\1.0""" -Wait -Passthru | Out-Null
 Write-Host "OUT"
+
+Write-Host "envs: $env:GITHUB_WORKSPACE"
+	
+# Install application
+Start-Process -FilePath "msiexec" -ArgumentList "/i ""$msiPath"" /passive /log ""$logFileName"" INSTALLDIR=""$programFilesDirectory\$applicationName\1.0""" -Wait -Passthru | Out-Null
