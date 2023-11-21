96ec3b35ce89f6c9694b975b63ae173bce40fd48
IrenMelisa <Beshenko.Irina@gmail.com>
2023-11-21 11:00:25 +0100

    Update BuildScript.ps1

--- ConsoleAppNew/ConsoleApp1/BuildScript.ps1
+++ ConsoleAppNew/ConsoleApp1/BuildScript.ps1
@@ -1,14 +1,15 @@
 $application = "ConsoleApp1"
 $msBuildExecutable = "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe"
-$projectsDirectory = "ConsoleAppNew"
+$projectsDirectory = "C:\actions-runner\_work\ConsoleRepo\ConsoleRepo\ConsoleAppNew\ConsoleApp1"
 $ApplicationStartDate = Get-Date
 $configuration = "Debug"
-
+$applicationLog = "c:\logTMP"
+echo $env:GITHUB_WORKSPACE
         Write-Host "Building $application" -ForegroundColor Cyan
 
 	$exitCode = (Start-Process `
 		-FilePath $msBuildExecutable `
-		-ArgumentList """$projectsDirectory\$application.sln"" $target /p:Configuration=$configuration /p:VisualStudioVersion=12.0 /fl /flp:logfile=$application.log;verbosity=normal" `
+		-ArgumentList """$projectsDirectory\$application.sln"" $target /p:Configuration=$configuration /p:VisualStudioVersion=12.0 /fl /flp:logfile=$applicationLog.log;verbosity=normal" `
 		-PassThru -Wait `
 	).ExitCode
 
