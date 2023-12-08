ea813ec996ec0a87bc731d97908a64cb8e5c5646
IrenMelisa <Beshenko.Irina@gmail.com>
2023-12-08 14:57:56 +0200

    Update DeployScript.ps1

--- ConsoleAppNew/ConsoleApp1/DeployScript.ps1
+++ ConsoleAppNew/ConsoleApp1/DeployScript.ps1
@@ -12,7 +12,13 @@ $applicationLog = "logTMP"
     # Set installer path
     $msiPath = "$msiFolder\Middleware.VFDE.I01-1.0.0.0.msi"
 
+try{
 throw "Bad thing happened"
+}
+catch
+{ Write-Host "catching errors"
+  echo "random_number=$(($RANDOM % 2))" >> $GITHUB_OUTPUT
+}
 
 Write-Host "envs: $env:GITHUB_WORKSPACE"
 $ComputerName = "dea059yw.eito-dublin.local"
