2868203b887ad4f7fcea14bd932ce97b6a63319f
IrenMelisa <Beshenko.Irina@gmail.com>
2023-12-08 15:03:57 +0200

    Update DeployScript.ps1

--- ConsoleAppNew/ConsoleApp1/DeployScript.ps1
+++ ConsoleAppNew/ConsoleApp1/DeployScript.ps1
@@ -14,10 +14,15 @@ $applicationLog = "logTMP"
 
 try{
 throw "Bad thing happened"
+Write-Host "do not play"
 }
 catch
 { Write-Host "catching errors"
   echo "random_number=$(($RANDOM % 2))" >> $GITHUB_OUTPUT
+  $a = "Hello, World"
+  return  $GITHUB_OUTPUT
+  Write-Host "do not play1"
+  
 }
 
 Write-Host "envs: $env:GITHUB_WORKSPACE"
