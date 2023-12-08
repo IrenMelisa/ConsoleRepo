7dc20dcd9d23d06675371275dba7ecfb49dfa83d
IrenMelisa <Beshenko.Irina@gmail.com>
2023-12-08 16:28:02 +0200

    Update faultrewrite.ps1

--- ConsoleAppNew/ConsoleApp1/faultrewrite.ps1
+++ ConsoleAppNew/ConsoleApp1/faultrewrite.ps1
@@ -3,7 +3,7 @@ throw "Bad thing happened"
 Write-Host "do not play"
 }
 catch
-{ Write-Host "catching errors"
+{ Write-Host "catching errors 66"
   echo "ERRCODE=66" >> $env:GITHUB_OUTPUT
   return  $GITHUB_OUTPUT
   Write-Host "do not play1"
