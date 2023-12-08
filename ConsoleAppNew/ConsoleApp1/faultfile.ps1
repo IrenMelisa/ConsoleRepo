375aa18ec82a7f13493b98222aca498828a11be0
IrenMelisa <Beshenko.Irina@gmail.com>
2023-12-08 16:20:20 +0200

    Update faultfile.ps1

--- ConsoleAppNew/ConsoleApp1/faultfile.ps1
+++ ConsoleAppNew/ConsoleApp1/faultfile.ps1
@@ -4,7 +4,7 @@ Write-Host "do not play"
 }
 catch
 { Write-Host "catching errors"
-  echo "errcode=55" >> $env:GITHUB_OUTPUT
+  echo "ERRCODE=55" >> $env:GITHUB_OUTPUT
   return  $GITHUB_OUTPUT
   Write-Host "do not play1"
   
