f6404926bc8ebd7068cdb3000a9e798b2edb7f7c
IrenMelisa <Beshenko.Irina@gmail.com>
2023-12-08 16:16:10 +0200

    Create faultfile.ps1

---
+++ ConsoleAppNew/ConsoleApp1/faultfile.ps1
@@ -0,0 +1,12 @@
+try{
+throw "Bad thing happened"
+Write-Host "do not play"
+}
+catch
+{ Write-Host "catching errors"
+  echo "random_number=$(($RANDOM % 2))" >> $GITHUB_OUTPUT
+  $a = "Hello, World"
+  return  $GITHUB_OUTPUT
+  Write-Host "do not play1"
+  
+}
